26bc498a9265b81b873117f6d2de9248df5191c4
Matkip <Matkip>
2023-03-07 14:34:30 +0100

    Update

--- Sorting Program/Program.cs
+++ Sorting Program/Program.cs
@@ -1,15 +1,27 @@
-﻿int[] arrayInts = { 10, 20, 50, 40, 30 };
+﻿int[] arrayInts = { 10, 20, 50, 40, 30,};
 
 int maxLength = arrayInts.Length;
 
 
 int[] newArray = QuickSort(arrayInts, maxLength);
 
+int arrayLength = newArray.Length;
+
+
+for (int i = 0; i < arrayLength; i++)
+{
+    Console.WriteLine(newArray[i]);
+}
+
+/* sorts an array from low to high
+ * uses a pivot at a random position in the array and makes an array with all the numbers smaller and an array with all the equal or bigger then the pivot
+ * it keeps doing it until there is only one number in each array and then puts all the numbers back together
+ */
 static int[] QuickSort(int[] arrayInts, int maxLength)
 {
     Random rng = new Random();
     int number = arrayInts[rng.Next(0, maxLength)];
-    int[] newArray = new int[maxLength * 2]; // Laver det nye array klar til at blive sent tilbage.
+    int[] newArray = new int[maxLength]; // Makes the new array that we are going to return
 
 
 
@@ -17,10 +29,12 @@ static int[] QuickSort(int[] arrayInts, int maxLength)
     int counterLower = 0;
     int counterUpper = 0;
 
+    //we makes 2 new Arrays, one for values lower then pivot and one for higher or equal pivot
     int[] lowerArray = new int[maxLength];
     int[] upperArray = new int[maxLength];
 
 
+    //sort the numbers into the temp arrays 
     foreach (int i in arrayInts)
     {
         if (i != 0)
@@ -39,19 +53,20 @@ static int[] QuickSort(int[] arrayInts, int maxLength)
         }
     }
 
+    //Does the QuickSort method again with the array with the lower values 
     if (counterLower > 1)
     {
         lowerArray = QuickSort(lowerArray, counterLower);
-
-
     }
 
+    //Does the QuickSort method again with the array with the higher values 
     if (counterUpper > 1)
     {
         upperArray = QuickSort(upperArray, counterUpper);
     }
 
 
+    //puts the values from the lowerArray in the newArray
     foreach (int j in lowerArray)
     {
         if (j != 0)
@@ -61,6 +76,7 @@ static int[] QuickSort(int[] arrayInts, int maxLength)
         }
     }
 
+    //puts the values from the upperArray in the newArray
     foreach (int j in upperArray)
     {
         if (j != 0)
@@ -71,14 +87,4 @@ static int[] QuickSort(int[] arrayInts, int maxLength)
     }
 
     return newArray;
-}
-
-
-
-int arrayLength = newArray.Length;
-
-
-for (int i = 0; i < arrayLength; i++)
-{
-    Console.WriteLine(newArray[i]);
 }
